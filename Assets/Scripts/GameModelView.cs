48d22070742c00b56d038b475cc98b04863b72c4
Jeremy <jeremymechen1997@gmail.com>
2017-10-07 21:21:28 +1300

    Project Setup + Design Document

---
+++ Assets/Scripts/GameModelView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModelView : MonoBehaviour, IGame {
+
+    public SceneController sceneController
+    {
+        get
+        {
+            return sceneController;
+        }
+
+        set
+        {
+            sceneController = value;
+        }
+    }
+
+}
---
+++ Assets/Scripts/IGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using UnityEngine;
+
+public interface IGame {
+
+    SceneController sceneController { get; set; }
+    // CameraController cameraController { get; set; }
+    // AudioController audioController { get; set; }
+
+}
